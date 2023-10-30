7070795ee76a6c8c343e582baa645f9fa9bdd3aa
Lars Eckart <lars.eckart@hey.com>
2023-10-30 22:15:21 +0200

    raise when chen above 10

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "always raise";
+    static final String VERSION = "raise when chen above 10";
 
     public static int betRequest(GameState gameState) {
-
+        if (gameState.me().score() > 10) {
+            return gameState.toRaiseByBlinds(2);
+        }
         return 0;
     }
 
